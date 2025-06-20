6b2c3300b658ceb5d1f82e315e3b974fe4312254
nhien261203 <dovannhien12345@gmail.com>
2025-06-20 09:06:38 +0700

    add sidebar admin

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../pages/admin/Header'
+import { Outlet } from 'react-router-dom'
+import SideBar from '../pages/admin/SideBar'
 
 
 const AdminLayout = () => {
+     const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+    const [collapsed, setCollapsed] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const mobile = window.innerWidth < 768;
+            setIsMobile(mobile);
+            if (mobile) setCollapsed(true);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const handleToggleSidebar = () => {
+        setCollapsed(prev => !prev);
+    };
+
     return (
         <>
             <div className="h-screen flex flex-col bg-gray-100 overflow-hidden ">
-                <Header />
+                <Header onToggleSidebar={handleToggleSidebar} />
+                {/* Phần còn lại cuộn cùng nhau */}
+                <div className="flex flex-1 overflow-hidden ">
+                    <SideBar collapsed={collapsed} isMobile={isMobile} />
 
+                    {/* Overlay mobile */}
+                    {isMobile && !collapsed && (
+                        <div
+                            className="fixed inset-0 bg-black bg-opacity-40 z-30 md:hidden"
+                            onClick={() => setCollapsed(true)}
+                        />
+                    )}
+
+                    
+
+
+                </div>
             </div>
 
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
 import './index.css'
 import App from './App.jsx'
 
@@ -11,6 +12,16 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <RouterProvider router={router} />
-    <ToastContainer position="top-right" autoClose={3000} />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </>
 )
